2c0b68dbb94732db64b3d2ac2a749e97ae5cdf3d
ManONES <manuel.romero@siessoftware.net>
2020-09-12 13:19:15 -0500

    Our F C

---
+++ src/main/java/com/manycode/app/DemoMicroservicios0601Application.java
@@ -0,0 +1,13 @@
+package com.manycode.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoMicroservicios0601Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoMicroservicios0601Application.class, args);
+	}
+
+}
---
+++ src/main/java/com/manycode/app/resources/UserResource.java
@@ -0,0 +1,60 @@
+package com.manycode.app.resources;
+
+import java.util.Random;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.reactive.function.client.WebClient;
+
+import com.manycode.app.model.User;
+
+@RestController
+public class UserResource {
+	private static Logger LOG = LoggerFactory.getLogger(UserResource.class);
+	
+    @Autowired
+    WebClient.Builder webClientBuilder;  
+
+    @RequestMapping("/user/{userId}")
+    public User getUserRatings(@PathVariable("userId") String userId) {
+    	int iii = 0;      
+		for (int ii = 0; ii < 10; ii++) {
+	        while (true) {
+			    iii =getRandomNumberInRange(1, 2000);
+			    LOG.info("intento:" + iii);
+	            if (iii==1974) { break;}
+	        }
+		}        	
+				
+		iniciodos();
+    	
+        User userRating = new User();
+        userRating.initData(userId + ":" +iii);
+        return userRating;
+    }
+    
+    public String iniciodos() {
+    	//UserRating userRating = webClientBuilder.build().get().uri("http://demo-microservicios-7-git-hola.apps.us-west-1.starter.openshift-online.com/user/MANYTHREE")
+    	User userRating = webClientBuilder.build().get().uri("http://demo-microservicios-7-git-hola.apps.us-west-1.starter.openshift-online.com/user/MANYTHREE")
+    			.retrieve().bodyToMono(User.class).block();          
+    	return (userRating.getUserId());    	
+    }
+      
+    
+    @GetMapping(value ="/testdestino")
+	public String welcome() {
+		return "Hola al mundo de Many  con  microservicios 1841";
+	}	
+    
+	private static int getRandomNumberInRange(int min, int max) {
+		
+		Random r = new Random();
+		return r.ints(min, (max + 1)).limit(1).findFirst().getAsInt();
+		
+	}	
+}
